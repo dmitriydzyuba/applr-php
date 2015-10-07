54bdea8ae29b0c9374611d613cb491f3273b46dd
Dmitriy Dzyuba <dmitriy.dzyuba@smoozeedev.com>
2015-10-07 15:39:02 +0300

    change API endpoint

--- API.php
+++ API.php
@@ -18,7 +18,7 @@ class API {
 	 * API endpoint base url
 	 */
 
-	const API_ENDPOINT = 'http://applr.io/api/v1/';
+	const API_ENDPOINT = 'https://applr.io/api/v1/';
 
 	/**
 	 * API key
@@ -92,6 +92,7 @@ class API {
 		curl_setopt($this->_ch, CURLOPT_CONNECTTIMEOUT, 20);
 		curl_setopt($this->_ch, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($this->_ch, CURLOPT_USERAGENT, 'applr-php');
+		curl_setopt($this->_ch, CURLOPT_SSL_VERIFYPEER, false);
 
 		if ($data) {
 			curl_setopt($this->_ch, CURLOPT_POST, true);
